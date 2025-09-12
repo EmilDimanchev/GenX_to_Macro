80eb8f3dc388146798e8d07c7bf0e3b840d8a566
Emil Dimanchev <emild@mit.edu>
2025-09-12 12:42:51 -0400

    now writing period_map.csv files

--- src/genx_conversion_fuctions/timedata.jl
+++ src/genx_conversion_fuctions/timedata.jl
@@ -20,7 +20,11 @@ function make_timedata_json(inputs::Dict, setup::Dict, commodities_vec::Vector{S
     return time_data
 end
 
-function make_timedata_json(inputs::Dict, setup::Dict, commodities_vec::Vector{String},genx_case_path::AbstractString, macro_case::AbstractString, genx_stage_path::AbstractString)
+# ~~~
+# Multistage version
+# ~~~
+
+function make_timedata_json(inputs::Dict, setup::Dict, commodities_vec::Vector{String}, genx_case_path::AbstractString, macro_case::AbstractString, genx_stage_path::AbstractString)
 
     stage_number = get_stage_number(genx_stage_path)
 
@@ -35,11 +39,30 @@ function make_timedata_json(inputs::Dict, setup::Dict, commodities_vec::Vector{S
 
     if setup["TimeDomainReduction"]==1
         time_data["TotalHoursModeled"] = sum(inputs["Weights"])
-        time_data["SubPeriodMap"] = Dict("path"=>string("system/Period_map",stage_number,".csv"))
+        time_data["SubPeriodMap"] = Dict("path"=>string("system/Period_map_",stage_number,".csv"))
     end
 
-    open(joinpath(macro_case,string("System/time_data",stage_number,".json")), "w") do io
+    open(joinpath(macro_case,string("System/time_data_",stage_number,".json")), "w") do io
         JSON3.pretty(io, time_data)
     end
+
+    # Copy period map
+    file_name = "Period_map.csv"
+    source_file_period_map = joinpath(genx_case_path, "inputs", string("inputs_p",stage_number), "TDR_results", file_name)
+    @info "period map file"
+    println(source_file_period_map)
+    destination_file_period_map = joinpath(macro_case,string("system/Period_map_",stage_number,".csv"))
+
+    # Check if the source file exists before copying
+    if isfile(source_file_period_map)
+        @info "Copying file: $source_file_period_map to $destination_file_period_map"
+        cp(source_file_period_map, destination_file_period_map; force=true)
+    else
+        @warn "File not found: $source_file_period_map"
+    end
+
+
     return time_data
+
+    
 end
